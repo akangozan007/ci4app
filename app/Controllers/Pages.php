3174bc62e5a240df995779217b7fe1821fe8c72d
razan rumah <razan@rumah.windows>
2025-04-03 16:59:17 +0700

    6. Views (Creating, and renderring data content ) layouts

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -4,34 +4,19 @@ namespace App\Controllers;
 
 class Pages extends BaseController
 {
-    // public function index(): string
-    // {
-    //     return view('welcome_message');
-    // }
-    // public function index()
-    // {
-    //     // menampilan file ci4app\app\Views\pages\homepage.php
-    //     return view('pages/homepage');
-    // }
-    // public function about()
-    // {
-    //     // menampilan file ci4app\app\Views\pages\homepage.php
-    //     return view('pages/about');
-    // }
-
+   
     public function index()
     {
-        // page home dengan header dan footer
-        echo view('layout/header');
-        echo view('pages/homepage');
-        echo view('layout/footer');
+        $data = ['title' => 'Laman | Beranda'];
+    
+        return view('pages/homepage', $data);
     }
+    
     public function about()
     {
-        // page about dengan header dan footer
-        echo view('layout/header');
-        echo view('pages/about');
-        echo view('layout/footer');
+    $data = ['title' => 'Laman | Tentang'];
+
+    return view('pages/about', $data);
     }
     
 }
